ec134794c0b13c5bef5f3554059d5d6041e38fbd
Abid <abidmohdasif@gmail.com>
2025-04-25 18:41:17 -0400

    Done i think

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const form = document.querySelector("form");
+let submit = document.getElementById("submit")
+let first = document.getElementById("firstName")
+let last = document.getElementById("lastName")
+let course = document.getElementById("course")
+let section = document.getElementById("sectionNsame")
+let role = document.getElementById("role")
 
-const person = {
-    firstName: "",
-    lastName: "",
-    course: "",
-    sectionNumber: "",
-    role: "",
+let person = {}
 
+submit.addEventListener("click", () =>  {
+    person.first = first.value
+    person.last = last.value
+    person.course = course.value
+    person.section = section.value
+    person.role = role.value
 
-}
+    console.log("Person Object: ")
 
+    console.log(person)
+
+    console.log("")
+
+    console.log("JSON format: ")
+
+    let x = JSON.stringify(person)
+    console.log(x)})
