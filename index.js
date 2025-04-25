45aa6d20e5e8f0598d821ffcf8784d65ba6042dd
Abid Asif <amasif@my.waketech.edu>
2025-04-25 12:02:04 -0400

    done

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+const form = document.querySelector("form");
 
-const user = {
-    firstName: "Abid",
-    lastName: "Asif",
-    course: " JavaScript",
-    sectionNumber: 115,
-    role: "student"
-};
-console.log("JavaScript Object:");
-console.log(user);
+const person = {
+    firstName: "",
+    lastName: "",
+    course: "",
+    sectionNumber: "",
+    role: "",
 
-const userJSON = JSON.stringify(user, null, 2);
-console.log("\nJSON Format:");
-console.log(userJSON);
+
+}
+
